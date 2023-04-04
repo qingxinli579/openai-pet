964528cf75de46d9fb3655f144ae244988575273
liqingxin <liqingxin@migu.cn>
2023-04-04 15:56:28 +0800

    test

--- test.js
+++ test.js
@@ -18,15 +18,15 @@ setTimeout(async () => {
     //   }).catch(err => {
     //     console.log(`错误：${err}`)
     //   });
-    openai.ChatCompletion(
-        model="gpt-3.5-turbo",
-        messages=[
+    openai.ChatCompletion({
+        model:"gpt-3.5-turbo",
+        messages:[
               {"role": "system", "content": "You are a helpful assistant."},
               {"role": "user", "content": "Who won the world series in 2020?"},
               {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
               {"role": "user", "content": "Where was it played?"}
           ]
-      ).then(res=>{
+        }).then(res=>{
         console.log(res['choices'][0]['message']['content'],JSON.stringify(res),'响应')
       })
 }, 2000)
